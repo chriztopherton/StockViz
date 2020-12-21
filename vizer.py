b9166e617dd664ba8a08dae0b98afdf9e3b01cc5
chriztopherton <christopherton1998@gmail.com>
2020-12-21 13:28:08 -0800

    remove lxml import

--- vizer.py
+++ vizer.py
@@ -6,8 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import datetime
 import time
-import pandasql as psql
-import lxml
 
 plt.style.use('seaborn-whitegrid')
 pd.options.mode.chained_assignment = None
